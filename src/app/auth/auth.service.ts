34f878bc65c6415e2659402ab4e3b9e484a4fa8c
Mohamed CHIBANI <41089034+mohamedchibani@users.noreply.github.com>
2023-03-12 12:18:08 +0100

    Using a Global Angular Config

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,8 +1,13 @@
-import { Router } from '@angular/router';
-import { AuthData } from './auth-data.model';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { AuthData } from './auth-data.model';
+
+import { environment } from './../../environments/environment';
+
+const BACKEND_URL = environment.apiUrl + '/user/';
 
 @Injectable({
   providedIn: 'root',
@@ -34,23 +39,21 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    return this.http
-      .post('http://localhost:3000/api/user/signup', authData)
-      .subscribe(
-        () => {
-          this.router.navigate(['/']);
-        },
-        (error) => {
-          this.authStatusListener.next(false);
-        }
-      );
+    return this.http.post(BACKEND_URL + 'signup', authData).subscribe(
+      () => {
+        this.router.navigate(['/']);
+      },
+      (error) => {
+        this.authStatusListener.next(false);
+      }
+    );
   }
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http
       .post<{ token: string; expiresIn: number; userId: string }>(
-        'http://localhost:3000/api/user/login',
+        BACKEND_URL + 'login',
         authData
       )
       .subscribe(
